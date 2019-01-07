09e9b30639c19b4ba34487b9f68e7a346f2c9e6d
zenott <mblevel@gmail.com>
2019-01-07 14:20:12 +0100

    Reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,35 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  const strArr = str.split('');
+  for (let i = 0; i < strArr.length / 2; i++) {
+    const temp = strArr[i];
+    strArr[i] = strArr[strArr.length - 1 - i];
+    strArr[strArr.length - 1 - i] = temp;
+  }
+  return strArr.join('');
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+//
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '');
+// }
