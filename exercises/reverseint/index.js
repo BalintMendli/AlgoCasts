19b22db2b9c38a97b9c55a1b3b53132d7284a05d
zenott <mblevel@gmail.com>
2019-01-11 18:52:43 +0100

    Integer reversal

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,39 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let nStr;
+  if (n < 0) {
+    nStr = n.toString();
+    const nStrSl = nStr.slice(1);
+    return Number(
+      '-' +
+        nStrSl
+          .split('')
+          .reverse()
+          .join('')
+    );
+  } else if (n === 0) {
+    return 0;
+  } else {
+    return Number(
+      n
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    );
+  }
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//   const reversed = n
+//     .toString()
+//     .split('')
+//     .reverse()
+//     .join('');
+
+//   return parseInt(reversed) * Math.sign(n);
+// }
