fbd9adfa8bf1f816c50cce88773e4eb11fba91db
zenott <mblevel@gmail.com>
2019-01-15 19:39:43 +0100

    MaxChars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const obj = {};
+  let max = 0;
+  let maxChar = '';
+  for (let i = 0; i < str.length; i++) {
+    if (obj[str.charAt(i)]) {
+      obj[str.charAt(i)]++;
+    } else {
+      obj[str.charAt(i)] = 1;
+    }
+    if (obj[str.charAt(i)] > max) {
+      max = obj[str.charAt(i)];
+      maxChar = str.charAt(i);
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+//   const charMap = {};
+//   let max = 0;
+//   let maxChar = '';
+
+//   for (let char of str) {
+//     if (charMap[char]) {
+//       charMap[char]++;
+//     } else {
+//       charMap[char] = 1;
+//     }
+//   }
+
+//   for (let char in charMap) {
+//     if (charMap[char] > max) {
+//       max = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+
+//   return maxChar;
+// }
