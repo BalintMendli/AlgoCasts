ea6c35d686573e5ea53169c472758d542069599a
zenott <mblevel@gmail.com>
2019-03-09 11:18:12 +0100

    Matrix spiral

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,104 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let matrix = [];
+  for (let i = 0; i < n; i++) {
+    matrix[i] = [];
+  }
+  let limitLowX = 0;
+  let limitHighX = n - 1;
+  let limitLowY = 0;
+  let limitHighY = n - 1;
+  let x = 0;
+  let y = 0;
+  let j = 1;
+  let dirArr = ['r', 'd', 'l', 'u'];
+  let cDir = 0;
+  let dir = dirArr[0];
+  matrix[0][0] = 1;
+  while (j < n * n) {
+    if (dir === 'r') {
+      x++;
+    }
+    if (dir === 'd') {
+      y++;
+    }
+    if (dir === 'l') {
+      x--;
+    }
+    if (dir === 'u') {
+      y--;
+    }
+    matrix[y][x] = j + 1;
+    if (dir === 'r' && x === limitHighX) {
+      limitLowY++;
+      cDir++;
+      dir = dirArr[cDir % 4];
+    }
+    if (dir === 'd' && y === limitHighY) {
+      limitHighX--;
+      cDir++;
+      dir = dirArr[cDir % 4];
+    }
+    if (dir === 'l' && x === limitLowX) {
+      limitHighY--;
+      cDir++;
+      dir = dirArr[cDir % 4];
+    }
+    if (dir === 'u' && y === limitLowY) {
+      limitLowX++;
+      cDir++;
+      dir = dirArr[cDir % 4];
+    }
+    j++;
+  }
+  return matrix;
+}
 
 module.exports = matrix;
+
+// function matrix(n) {
+//   const results = [];
+
+//   for (let i = 0; i < n; i++) {
+//     results.push([]);
+//   }
+
+//   let counter = 1;
+//   let startColumn = 0;
+//   let endColumn = n - 1;
+//   let startRow = 0;
+//   let endRow = n - 1;
+//   while (startColumn <= endColumn && startRow <= endRow) {
+//     // Top row
+//     for (let i = startColumn; i <= endColumn; i++) {
+//       results[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
+
+//     // Right column
+//     for (let i = startRow; i <= endRow; i++) {
+//       results[i][endColumn] = counter;
+//       counter++;
+//     }
+//     endColumn--;
+
+//     // Bottom row
+//     for (let i = endColumn; i >= startColumn; i--) {
+//       results[endRow][i] = counter;
+//       counter++;
+//     }
+//     endRow--;
+
+//     // start column
+//     for (let i = endRow; i >= startRow; i--) {
+//       results[i][startColumn] = counter;
+//       counter++;
+//     }
+//     startColumn++;
+//   }
+
+//   return results;
+// }
