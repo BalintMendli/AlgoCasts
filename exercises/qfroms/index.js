d1f6e2661b28c92876f022e9654922a50af488e2
zenott <mblevel@gmail.com>
2019-04-08 18:05:06 +0200

    Queue from stack

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,81 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stackForward = new Stack();
+    this.stackBackward = new Stack();
+    this.isBackwards = false;
+  }
+
+  add(n) {
+    if (this.isBackwards) {
+      while (this.stackBackward.peek()) {
+        this.stackForward.push(this.stackBackward.pop());
+      }
+    }
+    this.stackForward.push(n);
+    this.isBackwards = false;
+  }
+
+  remove() {
+    if (!this.isBackwards) {
+      while (this.stackForward.peek()) {
+        this.stackBackward.push(this.stackForward.pop());
+      }
+    }
+    this.isBackwards = true;
+    return this.stackBackward.pop();
+  }
+
+  peek() {
+    if (this.isBackwards == false) {
+      while (this.stackForward.peek()) {
+        this.stackBackward.push(this.stackForward.pop());
+      }
+    }
+    this.isBackwards = true;
+    return this.stackBackward.peek();
+  }
+}
 
 module.exports = Queue;
+
+// class Queue {
+//   constructor() {
+//     this.first = new Stack();
+//     this.second = new Stack();
+//   }
+
+//   add(record) {
+//     this.first.push(record);
+//   }
+
+//   remove() {
+//     while (this.first.peek()) {
+//       this.second.push(this.first.pop());
+//     }
+
+//     const record = this.second.pop();
+
+//     while (this.second.peek()) {
+//       this.first.push(this.second.pop());
+//     }
+
+//     return record;
+//   }
+
+//   peek() {
+//     while (this.first.peek()) {
+//       this.second.push(this.first.pop());
+//     }
+
+//     const record = this.second.peek();
+
+//     while (this.second.peek()) {
+//       this.first.push(this.second.pop());
+//     }
+
+//     return record;
+//   }
+// }
