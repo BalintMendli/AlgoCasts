1e0844d43f9770e9ef0dab4d63542834d009ab2a
zenott <mblevel@gmail.com>
2019-05-29 23:42:28 +0200

    Validating a Binary Search Tree

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,40 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node) {
+  if (!node) return true;
+  if (max(node.left) > node.data || min(node.right) < node.data) return false;
+  return validate(node.right) && validate(node.left);
+}
+
+function max(node) {
+  if (!node) return -Infinity;
+  return Math.max(max(node.left), max(node.right), node.data);
+}
+
+function min(node) {
+  if (!node) return Infinity;
+  return Math.min(min(node.left), min(node.right), node.data);
+}
 
 module.exports = validate;
+
+// function validate(node, min = null, max = null) {
+//   if (max !== null && node.data > max) {
+//     return false;
+//   }
+
+//   if (min !== null && node.data < min) {
+//     return false;
+//   }
+
+//   if (node.left && !validate(node.left, min, node.data)) {
+//     return false;
+//   }
+
+//   if (node.right && !validate(node.right, node.data, max)) {
+//     return false;
+//   }
+
+//   return true;
+// }
